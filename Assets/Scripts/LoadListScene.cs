bcd2bf7126ecc54575d495e26d8f830572872e9c
Haitham Ennasr <haitham@llcl.lc>
2016-01-28 12:54:26 -0500

    First Push - Introduction Level
    First push for the game, includes introduction level with button.

---
+++ Assets/Scripts/LoadListScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadListScene : MonoBehaviour {
+
+	//function to be called when the "Begin" button in the intro is clicked
+	public void BeginIsClicked (){
+		SceneManager.LoadScene ("Episode1");
+	}
+}
