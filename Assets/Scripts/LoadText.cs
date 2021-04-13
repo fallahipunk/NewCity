efed3499d6225b9bdacb6bcf2d622c25e6426d12
Osama Ennasr <2357864+ennasros@users.noreply.github.com>
2021-04-13 17:30:54 -0400

    added arabic text and  star pointer

--- Assets/Scripts/LoadText.cs
+++ Assets/Scripts/LoadText.cs
@@ -7,7 +7,9 @@ public class LoadText : MonoBehaviour
 {
     public int textIndex = 0;
     public TextAsset[] texts;
+    public TextAsset[] arabicTexts;
     public Text displayText;
+    public ArabicText displayTextArabic;
     public GameObject finalCube;
     public GameObject Reticle;
     public GameObject episodeCanvas;
@@ -15,12 +17,15 @@ public class LoadText : MonoBehaviour
     private void Awake()
     {
         displayText.text = texts[0].text;
+        displayTextArabic.Text = arabicTexts[0].text;
     }
 
     public void ContinueGame()
     {
+        Debug.Log("Getting clicked...");
         textIndex++;
         displayText.text = texts[textIndex].text;
+        displayTextArabic.Text = arabicTexts[textIndex].text;
         if (textIndex == texts.Length - 1)
         {
             finalCube.SetActive(true);
